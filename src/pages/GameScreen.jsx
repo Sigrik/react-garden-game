6ee9398d757ba9753c73140eaf937cbdd866d536
Sigrik <sigrikpoczta@gmail.com>
2024-05-12 16:12:44 +0200

    Add game grid generation

--- src/pages/GameScreen.jsx
+++ src/pages/GameScreen.jsx
@@ -1,3 +1,30 @@
+const gameGrid = [
+  [null, null, null, null],
+  [null, null, null, null],
+  [null, null, null, null],
+  [null, null, null, null],
+];
+
 export default function GameScreen() {
-  return <h1>Game Screen</h1>;
+  return (
+    <div className="flex flex-col flex-wrap items-center justify-center">
+      <ol className="flex flex-col flex-wrap justify-center">
+        {gameGrid.map((row, rowIndex) => (
+          <li key={rowIndex}>
+            <ol className="flex flex-wrap justify-center">
+              {row.map((col, colIndex) => (
+                <li
+                  key={colIndex}
+                  className="bg-grass-texture flex h-24 w-24 items-center justify-center bg-contain"
+                >
+                  Row:{rowIndex}
+                  <br /> Col:{colIndex}
+                </li>
+              ))}
+            </ol>
+          </li>
+        ))}
+      </ol>
+    </div>
+  );
 }
