85f4820487f032bd12aa475ef786c34429a93791
Sigrik <sigrikpoczta@gmail.com>
2024-05-14 19:22:39 +0200

    Refactor player controls to use state and lift them into GameScreen component

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,40 +1,7 @@
-import { useEffect } from "react";
-
-let playerPos = { row: 3, col: 0 };
-
-export function Player() {
-  useEffect(() => {
-    document.addEventListener("keydown", detectKeyDown, true);
-  }, []);
-
-  const detectKeyDown = (e) => {
-    switch (e.key) {
-      case "w":
-        playerPos.row += 1;
-        if (playerPos.row > 3) {
-          playerPos.row = 3;
-        }
-        break;
-      case "s":
-        playerPos.row -= 1;
-        if (playerPos.row < 0) {
-          playerPos.row = 0;
-        }
-        break;
-      case "a":
-        playerPos.col -= 1;
-        if (playerPos.col < 0) {
-          playerPos.col = 0;
-        }
-        break;
-      case "d":
-        playerPos.col += 1;
-        if (playerPos.col > 3) {
-          playerPos.col = 3;
-        }
-    }
-    console.log(playerPos);
-  };
-
-  return <div className="absolute">Player</div>;
+export function Player(props) {
+  return (
+    <div className="absolute" style={props.style}>
+      Player
+    </div>
+  );
 }
--- src/pages/GameScreen.jsx
+++ src/pages/GameScreen.jsx
@@ -1,4 +1,5 @@
 import { Player } from "../components/Player";
+import { useState, useEffect } from "react";
 
 const gameGrid = [
   [1, 0, 1, 0],
@@ -8,6 +9,61 @@ const gameGrid = [
 ];
 
 export default function GameScreen() {
+  const [playerPos, setPlayerPos] = useState({ row: 3, col: 0 });
+
+  function movePlayer(rowChange, colChange) {
+    setPlayerPos((prevPosition) => {
+      let newRow = prevPosition.row + rowChange;
+      let newCol = prevPosition.col + colChange;
+
+      if (newRow <= 0) {
+        newRow = 0;
+      } else if (newRow >= 3) {
+        newRow = 3;
+      }
+
+      if (newCol <= 0) {
+        newCol = 0;
+      } else if (newCol >= 3) {
+        newCol = 3;
+      }
+
+      return { row: newRow, col: newCol };
+    });
+  }
+
+  const detectKeyDown = (e) => {
+    switch (e.key) {
+      case "w":
+      case "ArrowUp":
+        movePlayer(-1, 0);
+        break;
+      case "s":
+      case "ArrowDown":
+        movePlayer(+1, 0);
+        break;
+      case "a":
+      case "ArrowLeft":
+        movePlayer(0, -1);
+        break;
+      case "d":
+      case "ArrowRight":
+        movePlayer(0, +1);
+    }
+  };
+
+  useEffect(() => {
+    console.log(playerPos); // Logs playerPos whenever it changes
+  }, [playerPos]);
+
+  useEffect(() => {
+    document.addEventListener("keydown", detectKeyDown, true);
+
+    return () => {
+      document.removeEventListener("keydown", detectKeyDown, true);
+    };
+  }, []);
+
   return (
     <div className="flex flex-col flex-wrap items-center justify-center">
       <ol className="flex flex-col flex-wrap justify-center">
