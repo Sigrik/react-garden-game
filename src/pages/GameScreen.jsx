1bd4e1ed4efcae8b0ec2116594272ca27ad713e0
Sigrik <sigrikpoczta@gmail.com>
2024-05-21 14:59:37 +0200

    Add function to check win condition

--- src/pages/GameScreen.jsx
+++ src/pages/GameScreen.jsx
@@ -14,11 +14,33 @@ export function GameScreen() {
     transform: `translate(${playerPos.col * 6}rem,${playerPos.row * 6}rem)`,
   };
 
+  function throttle(mainFunction, delay) {
+    let timerFlag = null;
+    return (...args) => {
+      if (timerFlag === null) {
+        mainFunction(...args);
+        timerFlag = setTimeout(() => {
+          timerFlag = null;
+        }, delay);
+      }
+    };
+  }
+
+  function checkWin(grid) {
+    const gridCount = grid.reduce((total, row) => {
+      return total + row.reduce((rowTotal, col) => rowTotal + col, 0);
+    }, 0);
+    if (gridCount === 16 || gridCount === 0) {
+      console.log("You won!");
+    }
+  }
+
+  checkWin(gameGrid);
+
   function movePlayer(rowChange, colChange) {
     setPlayerPos((prevPosition) => {
       let newRow = prevPosition.row + rowChange;
       let newCol = prevPosition.col + colChange;
-
       if (newRow <= 0) {
         newRow = 0;
       } else if (newRow >= 3) {
@@ -30,9 +52,17 @@ export function GameScreen() {
       } else if (newCol >= 3) {
         newCol = 3;
       }
-
+      if (newRow === prevPosition.row && newCol === prevPosition.col) {
+        console.log("row and col is the same, not moving");
+      } else {
+        swapSquare(newRow, newCol);
+      }
       return { row: newRow, col: newCol };
     });
+  }
+
+  function swapSquare(row, col) {
+    gameGrid[row][col] = gameGrid[row][col] === 1 ? 0 : 1;
   }
 
   const detectKeyDown = (e) => {
@@ -55,15 +85,18 @@ export function GameScreen() {
     }
   };
 
+  const throttledKeyDown = throttle(detectKeyDown, 300);
+  /*
   useEffect(() => {
     console.log(playerPos);
   }, [playerPos]);
+*/
 
   useEffect(() => {
-    document.addEventListener("keydown", detectKeyDown, true);
+    document.addEventListener("keydown", throttledKeyDown, true);
 
     return () => {
-      document.removeEventListener("keydown", detectKeyDown, true);
+      document.removeEventListener("keydown", throttledKeyDown, true);
     };
   }, []);
 
@@ -73,20 +106,15 @@ export function GameScreen() {
         <Player style={playerStyle} />
         {gameGrid.map((row, rowIndex) => (
           <li key={rowIndex}>
-            <ol className="flex flex-wrap justify-center">
-              {row.map((col, colIndex) =>
-                col ? (
-                  <li
-                    key={colIndex}
-                    className="bg-grass-texture flex h-24 w-24 items-center justify-center bg-contain"
-                  ></li>
-                ) : (
-                  <li
-                    key={colIndex}
-                    className="bg-mud-texture flex h-24 w-24 items-center justify-center bg-contain"
-                  ></li>
-                ),
-              )}
+            <ol className="flex flex-wrap justify-center transition-all">
+              {row.map((col, colIndex) => (
+                <li
+                  key={colIndex}
+                  className={`flex h-24 w-24 items-center justify-center bg-contain transition-all duration-300 ease-in-out ${
+                    col ? "bg-grass-texture" : "bg-mud-texture"
+                  }`}
+                ></li>
+              ))}
             </ol>
           </li>
         ))}
