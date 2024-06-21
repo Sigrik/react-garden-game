0abc38ed3b6a67d519a2b40742b6d965d30c7700
Sigrik <sigrikpoczta@gmail.com>
2024-06-21 14:56:58 +0200

    Add progress reset button

--- src/components/Settings.jsx
+++ src/components/Settings.jsx
@@ -1,5 +1,10 @@
 import { Modal } from "./Modal";
 
+function resetLevels() {
+  const completedLevels = [-1];
+  localStorage.setItem("completedLevels", JSON.stringify(completedLevels));
+}
+
 export function Settings({ onClose }) {
   return (
     <Modal zValue="40">
@@ -8,6 +13,12 @@ export function Settings({ onClose }) {
         <h1 className="text-5xl drop-shadow-px-btn">SETTINGS</h1>
         <p>Music Volume</p>
         <p>SFX Volume</p>
+        <button
+          onClick={resetLevels}
+          className="w-full rounded-md bg-amber-400 p-3 text-center text-3xl drop-shadow-px-btn hover:bg-amber-300"
+        >
+          Reset Progress
+        </button>
         <button
           onClick={onClose}
           className="absolute -right-2 top-0 h-16 w-16 text-center text-5xl"
